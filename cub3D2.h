7479cc7e94bc585543ddf752de6fd1d406af9659
Elisa neige CESPEDES <elcesped@paul-f5Ar2s3.clusters.42paris.fr>
2024-04-11 18:44:44 +0200

    parsing map in curse

--- cub3D2.h
+++ cub3D2.h
@@ -6,13 +6,14 @@
 /*   By: elcesped <elcesped@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 09:39:59 by npaolett          #+#    #+#             */
-/*   Updated: 2024/04/10 14:17:25 by elcesped         ###   ########.fr       */
+/*   Updated: 2024/04/11 15:00:32 by elcesped         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef cub3D2_H
 #define cub3D2_H
 
+#include "cub3D.h"
 # include "./libft/libft.h"
 //# include "../mlx_linux/mlx.h"
 # include "X11/X.h"      // verifier
@@ -20,30 +21,17 @@
 # include <math.h>
 #include <stdbool.h>
 
-typedef struct s_data
+
+typedef struct s_checkmap
 {
-	char	**map;
+	int	not_valid;
+	int	perso;
 	char	**check_map;
-	int		not_valid;
-	int		perso;
+}		t_checkmap;
 
-	int		play_x;
-	int		play_y;
-
-	int		col_x;
-	int		lin_y;
-
-	int		o;
-}				t_data;
-
-//Pour tester une map adaptation so_long
-int		ft_map_on_tab(int fd, t_data *data);
-char	*ft_ber_on_line(int fd, t_data *data);
-char	*ft_check_gnl(char *result, char *line, t_data *data, int fd);
-char	*ft_clean_gnl(char *line, char *result, int fd);
 
 // pour checker la map
-int		ft_check_map(t_data *data);
+int ft_check_map(t_cube *game);
 
 # define MLX_ERROR 1
 
