71f1b3848806301bd865c87ece724145d1e436d7
Elisa neige CESPEDES <elcesped@paul-f5Ar3s3.clusters.42paris.fr>
2024-04-23 19:20:57 +0200

    add minimap

--- minimap/minimap.c
+++ minimap/minimap.c
@@ -6,7 +6,7 @@
 /*   By: elcesped <elcesped@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:38:49 by elcesped          #+#    #+#             */
-/*   Updated: 2024/04/22 16:10:33 by elcesped         ###   ########.fr       */
+/*   Updated: 2024/04/23 18:56:50 by elcesped         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,42 +17,72 @@
 #define WIDTH  80
 #define HEIGHT 100
 
+//ca fait 9 par 7 case 
+
+
+//int ft_wall_on_minimap(t_cube *game)
+//{
+	
+//}
 
 int ft_put_minimap(t_cube *game)
 {
 	int colorA[3] = {0, 0, 0};
 	int colorB[3] = {255, 204, 204};
+	int colorC[3] = {204, 102, 51};
+	
+	int a = (int)game->pos_x;
+	int b = (int)game->pos_y;
+	int c = a - 3;
+	int d = b - 4;
 
 	int y;
 	y = 10;
 
 	int x;
 	x = 10;
+	//printf("le perso est ici : %d et %d\n", a, b);
+	
+	//if (a < )
 
 	while(1)
 	{
-		if((x < HEIGHT && y == 10) || (y < WIDTH && x == 10) || (y == WIDTH && x < HEIGHT) || (x == HEIGHT && y < WIDTH) || (x == 10 && y == 10))
-			game->buf[y][x] = (unsigned int)get_color(colorA);
-		else if (x == HEIGHT && y == WIDTH)
+		//ft_print_maperror(game, game->map_game, 0, 0);
+		//printf("c = %d d = %d et y = %d et x = %d\n", c, d, y, x);
+		//if((x < HEIGHT && y == 10) || (y < WIDTH && x == 10) || (y == WIDTH && x < HEIGHT) || (x == HEIGHT && y < WIDTH) || (x == 10 && y == 10))
+		//	game->buf[y][x] = (unsigned int)get_color(colorA);
+		if (x == HEIGHT && y == WIDTH)
 		{
 			game->buf[y][x] = (unsigned int)get_color(colorA);
 			break ;
-		}	
-		else if (x % 10 == 0 || y % 10 == 0 || x == 10)
+		}
+		else if (c < 0 || d < 0 || d >= game->crow || c >= game->l)
 			game->buf[y][x] = (unsigned int)get_color(colorA);
-
+		//else if (!game->map[c][d])
+		//	game->buf[y][x] = (unsigned int)get_color(colorA);			
+		else if ((x % 10 == 0 || y % 10 == 0) && c >= 0 && d >= 0 && game->map[c][d] && game->map[c][d] == '0')
+			game->buf[y][x] = (unsigned int)get_color(colorA);
+		else if(c >= 0 && d >= 0 && game->map[c][d] && game->map[c][d] == '1' && x <= HEIGHT && y <= WIDTH)
+			game->buf[y][x] = (unsigned int)get_color(colorA);
 		else
-			game->buf[y][x] = (unsigned int)get_color(colorB);
+		{
+			if (c == a && d == b)
+				game->buf[y][x] = (unsigned int)get_color(colorC);
+			else
+				game->buf[y][x] = (unsigned int)get_color(colorB);
+		}
+		if(x % 10 == 0 && x != 10)
+			d++;
 		if (x == HEIGHT)
 		{
-			x = 10;
+			x = 9;
 			y++;
+			d = b - 4;					
+			if (y % 10 == 0)
+				c++;
 		}
-		else
-			x++;
-		
+		x++;
 	}
-
     return 0;
 }
 
